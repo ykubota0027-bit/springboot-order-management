bd5f949b5ef15aa2c79491e961f6aba620273f92
ykubota0027 <ykubota0027@penguin>
2026-04-14 22:58:24 +0900

    一旦コミット

---
+++ backend/src/main/java/com/example/demo/model/Book.java
@@ -0,0 +1,94 @@
+package com.example.demo.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String author;
+    private BigDecimal price;
+    private LocalDate publishedDate;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Book(Long id, String title, String author, BigDecimal price, LocalDate publishedDate,
+            LocalDateTime createdAt) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.publishedDate = publishedDate;
+        this.createdAt = createdAt;
+        this.updatedAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDate getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(LocalDate publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
